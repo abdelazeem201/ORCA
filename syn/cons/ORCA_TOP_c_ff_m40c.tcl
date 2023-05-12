5775e304b326d1e8df04646b561a739f070aec27
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-05-13 02:22:36 +0300

    ORCA_TOP_c_ff_m40c.tcl

---
+++ syn/cons/ORCA_TOP_c_ff_m40c.tcl
@@ -0,0 +1,15 @@
+set_parasitic_parameters -early_spec minTLU -late_spec minTLU
+set_temperature -40
+set_process_number 1.01
+set_process_label fast
+set_voltage 0.95  -object_list VDD
+set_voltage 1.16  -object_list VDDH
+set_voltage 0.00  -object_list VSS
+
+set_timing_derate -late 1.05 -cell_delay -net_delay
+
+set_load 80 $orca_ports(pci_outputs)
+set_load 5 $orca_ports(sdram_outputs)
+set_load 5 $orca_ports(sd_ddr_outputs)
+set_load 20 $orca_ports(test_so)
+
