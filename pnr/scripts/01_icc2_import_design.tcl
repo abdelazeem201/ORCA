0efaeb9659b04ffbb3191b9988e9ff357b13b58d
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-05-13 02:38:07 +0300

    01_icc2_import_design.tcl

---
+++ pnr/scripts/01_icc2_import_design.tcl
@@ -0,0 +1,40 @@
+##############################################################
+# Function: read gate level netlist into ICC2
+# Created by Yanfuti
+##############################################################
+source scripts/00_common_initial_settings.tcl
+
+### variables
+set current_step "01_icc2_import"
+
+### create nlib
+file mkdir $nlib_dir
+file delete -force $nlib_dir/${design}_${current_step}.nlib
+create_lib -technology $synopsys_tech_tf -ref_libs $ndm_files $nlib_dir/${design}_${current_step}.nlib
+
+### read verilog
+read_verilog -library ${design}_${current_step}.nlib -design ${design} -top $design $import_netlists
+
+### initialization script
+source scripts/initialization_settings.tcl
+
+### load upf
+set_app_options -name mv.upf.enable_golden_upf -value true
+reset_upf
+load_upf $golden_upf
+commit_upf
+
+### connect pg
+connect_pg_net -automatic
+
+### save design
+save_block
+save_lib
+
+### exit icc2
+print_message_info
+quit!
+
+##############################################################
+# END
+##############################################################
