68df33ffc78d3a62d7031fec680f8a1792896039
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-05-13 02:43:19 +0300

    01_icc2_import_design.tcl

--- pnr/scripts/01_icc2_import_design.tcl
+++ pnr/scripts/01_icc2_import_design.tcl
@@ -1,6 +1,6 @@
 ##############################################################
 # Function: read gate level netlist into ICC2
-# Created by Yanfuti
+# Created by Ahmed Abdelazeem
 ##############################################################
 source scripts/00_common_initial_settings.tcl
 
