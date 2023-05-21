fa0ae9e1f605f2d8471009001832ed06f2128bdd
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-05-21 11:33:10 +0300

    PCI_W_MUX.vhd

---
+++ rtl/PCI_W_MUX.vhd
@@ -0,0 +1,72 @@
+----------------------------------------------------------------------/
+----                                                               ----
+----  The data contained in the file is created for educational    ---- 
+----  and training purposes only and is  not recommended           ----
+----  for fabrication                                              ----
+----                                                               ----
+----------------------------------------------------------------------/
+----                                                               ----
+----  Copyright (C) 2013 Synopsys, Inc.                            ----
+----                                                               ----
+----------------------------------------------------------------------/
+----                                                               ----
+----  The 32/28nm Generic Library ("Library") is unsupported       ----    
+----  Confidential Information of Synopsys, Inc. ("Synopsys")      ----    
+----  provided to you as Documentation under the terms of the      ----    
+----  End User Software License Agreement between you or your      ----    
+----  employer and Synopsys ("License Agreement") and you agree    ----    
+----  not to distribute or disclose the Library without the        ----    
+----  prior written consent of Synopsys. The Library IS NOT an     ----    
+----  item of Licensed Software or Licensed Product under the      ----    
+----  License Agreement.  Synopsys and/or its licensors own        ----    
+----  and shall retain all right, title and interest in and        ----    
+----  to the Library and all modifications thereto, including      ----    
+----  all intellectual property rights embodied therein. All       ----    
+----  rights in and to any Library modifications you make are      ----    
+----  hereby assigned to Synopsys. If you do not agree with        ----    
+----  this notice, including the disclaimer below, then you        ----    
+----  are not authorized to use the Library.                       ----    
+----                                                               ----  
+----                                                               ----      
+----  THIS LIBRARY IS BEING DISTRIBUTED BY SYNOPSYS SOLELY ON AN   ----
+----  "AS IS" BASIS, WITH NO INTELLECUTAL PROPERTY                 ----
+----  INDEMNIFICATION AND NO SUPPORT. ANY EXPRESS OR IMPLIED       ----
+----  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED       ----
+----  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR   ----
+----  PURPOSE ARE HEREBY DISCLAIMED. IN NO EVENT SHALL SYNOPSYS    ----
+----  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,     ----
+----  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      ----
+----  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     ----
+----  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)     ----
+----  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN    ----
+----  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    ----
+----  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      ----
+----  DOCUMENTATION, EVEN IF ADVISED OF THE POSSIBILITY OF         ----
+----  SUCH DAMAGE.                                                 ---- 		
+----                                                               ----  
+----------------------------------------------------------------------- 
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+ENTITY PCI_W_MUX IS
+  PORT (
+    blender_data      : IN  std_logic_vector(31 DOWNTO 0);
+    sdram_read_data   : IN  std_logic_vector(31 DOWNTO 0);
+    risc_result_data  : IN  std_logic_vector(31 DOWNTO 0);
+    pci_w_select      : IN  std_logic_vector(1 DOWNTO 0);
+    pci_wfifo_data    : OUT std_logic_vector(31 DOWNTO 0)
+  );
+
+END PCI_W_MUX;
+
+ARCHITECTURE RTL OF PCI_W_MUX IS
+BEGIN
+
+  WITH pci_w_select select
+    pci_wfifo_data <= blender_data WHEN "01",
+                      sdram_read_data WHEN "10",
+                      risc_result_data WHEN OTHERS;
+
+END RTL;
