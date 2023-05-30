07c61fae16ab10950a5c35358afa02311dd4e73a
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-05-30 12:24:58 +0300

    SDRAM_FIFO.vhd

--- rtl/SDRAM_FIFO.vhd
+++ rtl/SDRAM_FIFO.vhd
@@ -5,46 +5,7 @@
 ----  for fabrication                                              ----
 ----                                                               ----
 ----------------------------------------------------------------------/
-----                                                               ----
-----  Copyright (C) 2013 Synopsys, Inc.                            ----
-----                                                               ----
-----------------------------------------------------------------------/
-----                                                               ----
-----  The 32/28nm Generic Library ("Library") is unsupported       ----    
-----  Confidential Information of Synopsys, Inc. ("Synopsys")      ----    
-----  provided to you as Documentation under the terms of the      ----    
-----  End User Software License Agreement between you or your      ----    
-----  employer and Synopsys ("License Agreement") and you agree    ----    
-----  not to distribute or disclose the Library without the        ----    
-----  prior written consent of Synopsys. The Library IS NOT an     ----    
-----  item of Licensed Software or Licensed Product under the      ----    
-----  License Agreement.  Synopsys and/or its licensors own        ----    
-----  and shall retain all right, title and interest in and        ----    
-----  to the Library and all modifications thereto, including      ----    
-----  all intellectual property rights embodied therein. All       ----    
-----  rights in and to any Library modifications you make are      ----    
-----  hereby assigned to Synopsys. If you do not agree with        ----    
-----  this notice, including the disclaimer below, then you        ----    
-----  are not authorized to use the Library.                       ----    
-----                                                               ----  
-----                                                               ----      
-----  THIS LIBRARY IS BEING DISTRIBUTED BY SYNOPSYS SOLELY ON AN   ----
-----  "AS IS" BASIS, WITH NO INTELLECUTAL PROPERTY                 ----
-----  INDEMNIFICATION AND NO SUPPORT. ANY EXPRESS OR IMPLIED       ----
-----  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED       ----
-----  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR   ----
-----  PURPOSE ARE HEREBY DISCLAIMED. IN NO EVENT SHALL SYNOPSYS    ----
-----  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,     ----
-----  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      ----
-----  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     ----
-----  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)     ----
-----  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN    ----
-----  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    ----
-----  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      ----
-----  DOCUMENTATION, EVEN IF ADVISED OF THE POSSIBILITY OF         ----
-----  SUCH DAMAGE.                                                 ---- 		
-----                                                               ----  
------------------------------------------------------------------------ 
+ 
 
 
 library IEEE,DWARE,DW06;
